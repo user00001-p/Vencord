46a2c3d4eedfa83da3a926ab20bd6dd15b8d3d80
viciouscal <78273450+viciouscal@users.noreply.github.com>
2024-12-22 04:31:50 +0300

    Add files via upload

---
+++ src/plugins/betterMicrophone/index.ts
@@ -0,0 +1,48 @@
+import { Devs } from "@utils/constants";
+import definePlugin, { OptionType } from "@utils/types";
+import { definePluginSettings } from "@api/Settings";
+
+const settings = definePluginSettings({
+    stereochannel: {
+        description: "Stereo Channel",
+        type: OptionType.SELECT,
+        options: [
+            { label: "1.0 Mono", value: 1 },
+            { label: "2.0 Stereo", value: 2 },
+            { label: "7.1 Surround", value: 7.1, default: true },
+        ],
+    }
+});
+
+export default definePlugin({
+    name: "betterMicrophone",
+    description: "This plugin allows you to further customize your microphone.",
+    authors: [Devs.viciouscal],
+    settings,
+    patches: [
+        {
+            find: "Audio codecs",
+            replacement: {
+                match: /channels\:1\,/,
+                replace: `channels:1,prams:{stereo:\"1\"},`,
+                predicate: () => settings.store.stereochannel === 1
+            }
+        },
+        {
+            find: "Audio codecs",
+            replacement: {
+                match: /channels\:1\,/,
+                replace: `channels:2,prams:{stereo:\"2\"},`,
+                predicate: () => settings.store.stereochannel === 2
+            }
+        },
+        {
+            find: "Audio codecs",
+            replacement: {
+                match: /channels\:1\,/,
+                replace: `channels:7.1,prams:{stereo:\"7.1\"},`,
+                predicate: () => settings.store.stereochannel === 7.1
+            }
+        }
+    ]
+});
